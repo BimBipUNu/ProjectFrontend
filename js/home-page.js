7dc7a84e51263cab82c10c2219abcfb61ee4e7cd
BimBipUNu <trantai180506@gmail.com>
2025-04-24 01:08:25 +0700

    Task 1

--- js/home-page.js
+++ js/home-page.js
@@ -2,19 +2,13 @@ const toastEl = document.getElementById('successToast');
 const toast = new bootstrap.Toast(toastEl);
 toast.show();
 
-//Thay đổi màu cho hiệu ứng trang hiện tại trên nav
-const sidebar = document.getElementById("sidebar");
-sidebar.addEventListener("click", function(e){
-    const target = e.target.closest('a');
-    if (target) {
-        target.classList.add('currPage');
-    }
-})
-// js cho sidebar ẩn hiệnhiện
-const handleNav = document.getElementById("handleNav");
-handleNav.addEventListener("click", function() {
-    const sidebar = document.getElementById("sidebar");
-    sidebar.style.display = "none"
-    const content = document.getElementById("content");
-    content.style.left = "0"
-})
+//Hiệu ứng cho sidebar
+const sidebarCol = document.getElementById('sidebarCol');
+const mainContent = document.getElementById('mainContent');
+const toggleBtn = document.getElementById('toggleBtn');
+
+toggleBtn.addEventListener('click', () => {
+    sidebarCol.classList.toggle('collapsed');
+    mainContent.classList.toggle('col-10');
+    mainContent.classList.toggle('col-12');
+});
