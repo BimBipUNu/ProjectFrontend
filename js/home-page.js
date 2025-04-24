aad5a25fd97bdc23009c6439d20a8d90f763b201
BimBipUNu <trantai180506@gmail.com>
2025-04-24 20:26:21 +0700

    Task 2

--- js/home-page.js
+++ js/home-page.js
@@ -2,13 +2,3 @@ const toastEl = document.getElementById('successToast');
 const toast = new bootstrap.Toast(toastEl);
 toast.show();
 
-//Hiệu ứng cho sidebar
-const sidebarCol = document.getElementById('sidebarCol');
-const mainContent = document.getElementById('mainContent');
-const toggleBtn = document.getElementById('toggleBtn');
-
-toggleBtn.addEventListener('click', () => {
-    sidebarCol.classList.toggle('collapsed');
-    mainContent.classList.toggle('col-10');
-    mainContent.classList.toggle('col-12');
-});
---
+++ js/sidebar.js
@@ -0,0 +1,10 @@
+//Hiệu ứng cho sidebar
+const sidebarCol = document.getElementById('sidebarCol');
+const mainContent = document.getElementById('mainContent');
+const toggleBtn = document.getElementById('toggleBtn');
+
+toggleBtn.addEventListener('click', () => {
+    sidebarCol.classList.toggle('collapsed');
+    mainContent.classList.toggle('col-10');
+    mainContent.classList.toggle('col-12');
+});
